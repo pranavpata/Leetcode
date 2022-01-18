ed07aaaeced6ed4c754181c27510f82a40a8bbfc
pranavpata <pranavpata2@gmail.com>
2022-01-17 16:51:30 -0800

    Time: 42 ms (18.90%), Space: 51.4 MB (19.63%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        Stack<Integer> stack = new Stack<>();
+        Set<Integer> set = new HashSet<>();
+
+        for(int i = 0; i < s.length(); i++) {
+          if(s.charAt(i) == '(') {
+            stack.push(i);
+          } else if(s.charAt(i) == ')') {
+            if(stack.isEmpty()) {
+              set.add(i);
+            } else {
+              stack.pop();
+            }
+          }
+        }
+
+        while(!stack.isEmpty()) {
+          set.add(stack.pop());
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < s.length(); i++) {
+          if(!set.contains(i)) {
+            sb.append(s.charAt(i));
+          }
+        }
+
+        return sb.toString();
+    }
+}
