7d4a93d3a7e7fea8ef3032efe0da4fd7fae28b40
pranavpata <pranavpata2@gmail.com>
2022-01-19 11:26:28 -0800

    Time: 7 ms (91.12%), Space: 13.6 MB (24.60%) - LeetHub

--- 280-wiggle-sort/280-wiggle-sort.cpp
+++ 280-wiggle-sort/280-wiggle-sort.cpp
@@ -11,11 +11,11 @@ public:
     for (int i = 0; i < nums.size() - 1; i++) {
         if (less) {
             if (nums[i] > nums[i + 1]) {
-                mySwap(nums, i, i + 1);
+                swap(nums[i], nums[i + 1]);
             }
         } else {
             if (nums[i] < nums[i + 1]) {
-                mySwap(nums, i, i + 1);
+                swap(nums[i], nums[i + 1]);
             }
         }
         less = !less;
